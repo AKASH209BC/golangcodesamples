67ce2de00b30ec629d0a20a5af77812afb341119
Akash Sharma <akash24497@gmail.com>
2021-05-16 17:55:00 +0530

    Adding Ninja Exercise Part 1

---
+++ ninja_exercises/declarevariable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var x = 42
+var y = "James Bond"
+var z = true
+
+func main(){
+	fmt.Println(x,y,z)
+	fmt.Println("The value of x is ", x)
+	fmt.Println("The value of y is ", y)
+	fmt.Println("The value of z is ", z)
+}
