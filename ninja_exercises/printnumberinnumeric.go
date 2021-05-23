d1329ee8f353d424dc585f1469b4d5f5c4c9cd3a
Akash Sharma <akash24497@gmail.com>
2021-05-23 22:28:59 +0530

    Added Print in different formats

---
+++ ninja_exercises/printnumberinnumeric.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	x:=48
+	// to print in decimal use %d
+	fmt.Printf("Number in decimal is %d\n",x)
+	// to print in binary use %b
+	fmt.Printf("Number in binary is %b\n",x)
+	// to print in hexadecimal use %#x
+	fmt.Printf("Number in hexadecimal is %#x",x)
+}
