7e089ddaf717c09fb5671262d657294f4eec65d8
Akash Sharma <akash24497@gmail.com>
2021-05-23 23:22:40 +0530

    Using string value

---
+++ ninja_exercises/usingstringvalue.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	// `""` helps to create a raw string literal
+	// "" can also be used to create string
+	x := `"Hello, My name is Akash"`
+	y := "Hi there"
+	fmt.Println(x)
+	fmt.Println(y)
+}
